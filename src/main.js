92bcc2d6f774dd49cdcb47ed6b86605b6448fb74
lhsheild <lhsheild@yahoo.com>
2019-07-13 18:10:53 +0800

    home swiper

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwaiper from 'vue-awesome-swiper'
 import fastClick from 'fastclick'
 import 'reset-css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = true
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwaiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.key">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '001',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20193/87a224d0349d94a11e97f31aa1aba4f5.jpg_750x200_1f78af87.jpg'
+        },
+        {
+          id: '002',
+          imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20195/5eb82a8ca5b0cebb2c3ae1a53a4d8f81.jpg_750x200_645bfba1.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+  .wrapper >>> .swiper-pagination-bullet-active
+    background #fff !important
+
+  .wrapper
+    overflow hidden
+    width 100%
+    height: 0
+    padding-bottom 26.68%
+    background #eee
+    .swiper-img
+      width 100%
+</style>
