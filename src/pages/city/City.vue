9507e1bcf2c9936bf21468f99dbeb886775754fb
lhsheild <lhsheild@yahoo.com>
2019-07-16 09:03:16 +0800

    city components

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,8 +2,8 @@
     <div>
       <city-header></city-header>
       <city-search></city-search>
-      <city-list :cities="cities" :hotCities="hotCities"></city-list>
-      <city-alphabet :cities="cities"></city-alphabet>
+      <city-list :cities="cities" :hotCities="hotCities" :letter="letter"></city-list>
+      <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
     </div>
 </template>
 
@@ -19,7 +19,8 @@ export default {
   data () {
     return {
       cities: {},
-      hotCities: []
+      hotCities: [],
+      letter: ''
     }
   },
   components: {
@@ -36,12 +37,14 @@ export default {
     },
     handleGetCityInfoSucc (res) {
       res = res.data
-      console.log(res)
       if (res.ret && res.data) {
         const data = res.data
         this.cities = data.cities
         this.hotCities = data.hotCities
       }
+    },
+    handleLetterChange (letter) {
+      this.letter = letter
     }
   },
   mounted () {
--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,8 +1,11 @@
 <template>
-<div>
-  <ul class="list">
-  <li class="item" v-for="(item, key) of cities" :key="key" @click="handleLetterClick">{{key}}</li>
-</ul></div>
+  <div>
+    <ul class="list">
+      <li class="item" v-for="item of letters" :key="item" @click="handleLetterClick" :ref="item"
+          @touchstart="handleTouchStart" @touchmove="handleTouchMove" @touchend="handleTouchEnd">{{item}}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -11,9 +14,46 @@ export default {
   props: {
     cities: Object
   },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
   methods: {
     handleLetterClick (e) {
-      console.log(e.target.innerText)
+      this.$emit('change', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 66.53
+          const index = Math.floor((touchY - this.startY) / 21) + 1
+          if (index >= 0 && index < this.letters.length) { this.$emit('change', this.letters[index]) }
+        }, 5)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
     }
   }
 }
