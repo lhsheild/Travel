520dcb270ba80011ad12997e5ffb019ae77f7a77
lhsheild <lhsheild@yahoo.com>
2019-07-18 23:54:33 +0800

    detail-banner 布局和CSS

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -3,7 +3,7 @@
       <img class="banner-img" src="http://img1.qunarzz.com/sight/p0/1806/da/da0f9ef498e8b1f8a3.img.jpg_600x330_c188d4ac.jpg" alt="">
       <div class="banner-info">
         <div class="banner-title">阿狸欢乐王国南宁站</div>
-        <div class="banner-number"><span class="iconfont banner-icon">&#xe6aa;</span>9</div>
+        <div class="banner-number"><span class="iconfont banner-icon">&#xe63e;</span>9</div>
       </div>
     </div>
 </template>
@@ -30,18 +30,20 @@ export default {
     line-height 1.5rem
     display flex
     color: #fff
+    background-image linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,0.8))
     .banner-title
       flex 1
-      font-size 0.32rem
+      font-size 0.7rem
       padding: 0 0.2rem
     .banner-number
-      margin-top 0.2rem
+      margin-top 0.6rem
       padding:0 .4rem
-      line-height 0.24rem
+      line-height 0.7rem
       height:0.24rem
       border-radius 0.2rem
       background rgba(0,0,0,0.8)
       font-size 0.2rem
       .banner-icon
         font-size 0.24rem
+        padding-right 0.3rem
 </style>
